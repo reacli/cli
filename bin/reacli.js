fd8e10ebf2481552ac803c86248c16291c1a85b6
Gautier Darchen <darchen.gautier@gmail.com>
2019-02-11 23:31:09 +0100

    feat: flow arg is implemented to optionnaly add flow in new components

--- bin/reacli.js
+++ bin/reacli.js
@@ -86,14 +86,14 @@ const createFiles = (path, componentName, dumbString, containerString, indexStri
 }
 
 const parseDumbComponent = (componentName) => {
-	const filePath = makePath("./patterns/my-component/components/MyComponent.jsx")
+	const filePath = makePath("./patterns/my-component/components/MyComponent.template")
 	const data = fs.readFileSync(filePath).toString()
 
 	return data.replace(/MyComponent/gu, componentName)
 }
 
 const parseContainer = (componentName) => {
-	const filePath = makePath("./patterns/my-component/components/MyComponentContainer.jsx")
+	const filePath = makePath("./patterns/my-component/components/MyComponentContainer.template")
 	const data = fs.readFileSync(filePath).toString()
 
 	return data.replace(/MyComponent/gu, componentName)
@@ -106,7 +106,45 @@ const parseIndex = (componentName) => {
 	return data.replace(/MyComponent/gu, componentName)
 }
 
-const createComponent = (path) => {
+const applyFlowOption = (dumbString, containerString, componentName) => {
+	containerString = containerString.replace(/\[\[flow-declaration\]\]/gu, "// @flow")
+	containerString = containerString.replace(/\[\[flow-props-type\]\]/gu, "type Props = {\n\n};");
+	containerString = containerString.replace(/\[\[flow-state-type\]\]/gu, "type State = {\n\tvalue1: string,\n};");
+	containerString = containerString.replace(/\[\[flow-component-typing\]\]/gu, "<Props, State>");
+	containerString = containerString.replace(/\[\[flow-default-props-static\]\]/gu, "static defaultProps = {\n\n\t};");
+
+	dumbString = dumbString.replace(/\[\[flow-declaration\]\]/gu, "// @flow")
+	dumbString = dumbString.replace(/\[\[flow-props-type\]\]/gu, "type Props = {\n\tvalue1?: string,\n};");
+	dumbString = dumbString.replace(/\[\[flow-dumb-component-props-typing\]\]/gu, ": Props")
+	dumbString = dumbString.replace(/\[\[flow-default-props-out\]\]/gu, `${componentName}.defaultProps = {\n\tvalue1: '',\n};`);
+
+	return [
+		dumbString,
+		containerString,
+	]
+}
+
+const dismissFlowOption = (dumbString, containerString) => {
+	containerString = containerString.replace(/\[\[flow-declaration\]\]/gu, "").trim()
+	containerString = containerString.replace(/\[\[flow-props-type\]\]/gu, "").trim()
+	containerString = containerString.replace(/\[\[flow-state-type\]\]/gu, "").trim()
+	containerString = containerString.replace(/\[\[flow-component-typing\]\]/gu, "")
+	containerString = containerString.replace(/\[\[flow-default-props-static\]\]/gu, "").trim()
+	// Remove empty lines : containerString = containerString.replace(/^\s*[\r\n\n]/gm, "\n")
+
+	dumbString = dumbString.replace(/\[\[flow-declaration\]\]/gu, "").trim()
+	dumbString = dumbString.replace(/\[\[flow-props-type\]\]/gu, "").trim()
+	dumbString = dumbString.replace(/\[\[flow-dumb-component-props-typing\]\]/gu, "")
+	dumbString = dumbString.replace(/\[\[flow-default-props-out\]\]/gu, "").trim()
+	// Remove empty lines : dumbString = dumbString.replace(/^\s*[\r\n\n]/gm, '\n')
+
+	return [
+		dumbString,
+		containerString,
+	]
+}
+
+const createComponent = (path, options) => {
 	const folderName = getFolderName(path)
 	const componentName = makeComponentName(folderName)
 
@@ -116,24 +154,37 @@ const createComponent = (path) => {
 	fs.mkdirSync(path)
 	fs.mkdirSync(componentsPath)
 
-	const dumbString = parseDumbComponent(componentName)
-	const containerString = parseContainer(componentName)
+	let dumbString = parseDumbComponent(componentName)
+	let containerString = parseContainer(componentName)
 	const indexString = parseIndex(componentName)
+
+	if (options.flow) {
+		[dumbString, containerString] = applyFlowOption(dumbString, containerString, componentName)
+	} else {
+		[dumbString, containerString] = dismissFlowOption(dumbString, containerString)
+	}
 
 	createFiles(path, componentName, dumbString, containerString, indexString)
 }
 
 const reactCli = (args) => {
 	const firstParam = args.shift()
-  const secondParam = args.shift()
+	const secondParam = args.shift()
 
-  // Cmd reacli component <path> creates a component architecture
+	let options = {}
+	const useFlow = (/--flow|-f/u).test(args)
+
+	if (useFlow) {
+		options = Object.assign(options, { flow: true })
+	}
+
+   // Cmd reacli component <path> creates a component architecture
 	if (firstParam === "component") {
 		const path = makePath(secondParam)
 
 		if (validatePath(path) && validateName(path)) {
 			try {
-				createComponent(path)
+				createComponent(path, options)
 			} catch (error) {
 				console.log("ERROR: ", error)
 			}
--- dist/bin/reacli.js
+++ dist/bin/reacli.js
@@ -5,6 +5,8 @@ Object.defineProperty(exports, "__esModule", {
 	value: true
 });
 
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
+
 var _fs = require("fs");
 
 var _fs2 = _interopRequireDefault(_fs);
@@ -102,14 +104,14 @@ var createFiles = function createFiles(path, componentName, dumbString, containe
 };
 
 var parseDumbComponent = function parseDumbComponent(componentName) {
-	var filePath = makePath("./patterns/my-component/components/MyComponent.jsx");
+	var filePath = makePath("./patterns/my-component/components/MyComponent.template");
 	var data = _fs2.default.readFileSync(filePath).toString();
 
 	return data.replace(/MyComponent/g, componentName);
 };
 
 var parseContainer = function parseContainer(componentName) {
-	var filePath = makePath("./patterns/my-component/components/MyComponentContainer.jsx");
+	var filePath = makePath("./patterns/my-component/components/MyComponentContainer.template");
 	var data = _fs2.default.readFileSync(filePath).toString();
 
 	return data.replace(/MyComponent/g, componentName);
@@ -122,7 +124,39 @@ var parseIndex = function parseIndex(componentName) {
 	return data.replace(/MyComponent/g, componentName);
 };
 
-var createComponent = function createComponent(path) {
+var applyFlowOption = function applyFlowOption(dumbString, containerString, componentName) {
+	containerString = containerString.replace(/\[\[flow\x2Ddeclaration\]\]/g, '// @flow');
+	containerString = containerString.replace(/\[\[flow\x2Dprops\x2Dtype\]\]/g, "type Props = {\n\n};");
+	containerString = containerString.replace(/\[\[flow\x2Dstate\x2Dtype\]\]/g, "type State = {\n\tvalue1: string,\n};");
+	containerString = containerString.replace(/\[\[flow\x2Dcomponent\x2Dtyping\]\]/g, "<Props, State>");
+	containerString = containerString.replace(/\[\[flow\x2Ddefault\x2Dprops\x2Dstatic\]\]/g, "static defaultProps = {\n\n\t};");
+
+	dumbString = dumbString.replace(/\[\[flow\x2Ddeclaration\]\]/g, '// @flow');
+	dumbString = dumbString.replace(/\[\[flow\x2Dprops\x2Dtype\]\]/g, "type Props = {\n\tvalue1?: string,\n};");
+	dumbString = dumbString.replace(/\[\[flow\x2Ddumb\x2Dcomponent\x2Dprops\x2Dtyping\]\]/g, ': Props');
+	dumbString = dumbString.replace(/\[\[flow\x2Ddefault\x2Dprops\x2Dout\]\]/g, componentName + ".defaultProps = {\n\tvalue1: '',\n};");
+
+	return [dumbString, containerString];
+};
+
+var dismissFlowOption = function dismissFlowOption(dumbString, containerString) {
+	containerString = containerString.replace(/\[\[flow\x2Ddeclaration\]\]/g, '').trim();
+	containerString = containerString.replace(/\[\[flow\x2Dprops\x2Dtype\]\]/g, '').trim();
+	containerString = containerString.replace(/\[\[flow\x2Dstate\x2Dtype\]\]/g, '').trim();
+	containerString = containerString.replace(/\[\[flow\x2Dcomponent\x2Dtyping\]\]/g, '');
+	containerString = containerString.replace(/\[\[flow\x2Ddefault\x2Dprops\x2Dstatic\]\]/g, '').trim();
+	containerString = containerString.replace(/^\s*[\r\n\n]/gm, '\n');
+
+	dumbString = dumbString.replace(/\[\[flow\x2Ddeclaration\]\]/g, '').trim();
+	dumbString = dumbString.replace(/\[\[flow\x2Dprops\x2Dtype\]\]/g, '').trim();
+	dumbString = dumbString.replace(/\[\[flow\x2Ddumb\x2Dcomponent\x2Dprops\x2Dtyping\]\]/g, '');
+	dumbString = dumbString.replace(/\[\[flow\x2Ddefault\x2Dprops\x2Dout\]\]/g, '').trim();
+	dumbString = dumbString.replace(/^\s*[\r\n\n]/gm, '\n');
+
+	return [dumbString, containerString];
+};
+
+var createComponent = function createComponent(path, options) {
 	var folderName = getFolderName(path);
 	var componentName = makeComponentName(folderName);
 
@@ -136,6 +170,22 @@ var createComponent = function createComponent(path) {
 	var containerString = parseContainer(componentName);
 	var indexString = parseIndex(componentName);
 
+	if (options.flow) {
+		var _applyFlowOption = applyFlowOption(dumbString, containerString, componentName);
+
+		var _applyFlowOption2 = _slicedToArray(_applyFlowOption, 2);
+
+		dumbString = _applyFlowOption2[0];
+		containerString = _applyFlowOption2[1];
+	} else {
+		var _dismissFlowOption = dismissFlowOption(dumbString, containerString);
+
+		var _dismissFlowOption2 = _slicedToArray(_dismissFlowOption, 2);
+
+		dumbString = _dismissFlowOption2[0];
+		containerString = _dismissFlowOption2[1];
+	}
+
 	createFiles(path, componentName, dumbString, containerString, indexString);
 };
 
@@ -143,13 +193,20 @@ var reactCli = function reactCli(args) {
 	var firstParam = args.shift();
 	var secondParam = args.shift();
 
+	var options = {};
+	var useFlow = /--flow|-f/.test(args);
+
+	if (useFlow) {
+		options = Object.assign(options, { flow: true });
+	}
+
 	// Cmd reacli component <path> creates a component architecture
 	if (firstParam === "component") {
 		var path = makePath(secondParam);
 
 		if (validatePath(path) && validateName(path)) {
 			try {
-				createComponent(path);
+				createComponent(path, options);
 			} catch (error) {
 				console.log("ERROR: ", error);
 			}
