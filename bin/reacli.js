f8f1238c567a853abd0507b6b2a4f4d16b8f48a8
Gautier Darchen <darchen.gautier@gmail.com>
2019-02-25 16:51:58 +0100

    feat: enable interactive CLI

--- bin/reacli.js
+++ bin/reacli.js
@@ -3,30 +3,10 @@
 import pathModule from "path"
 import program from "commander"
 import pkgInfo from "pkginfo"
-import inquirer from "inquirer";
 
-import { validateName, validatePath } from "../lib/utils/validators"
 import { createComponent } from "../lib/core"
+import { interactiveCLI } from "../lib/interactiveCLI";
 
-
-const askQuestions = () => {
-	const questions = [
-		{
-			choices: ["Component", "Hook"],
-			message: "What would you like to create?",
-			name: "TYPE",
-			type: "list",
-		},
-		{
-			message: "Enter the path where you would like your component to be created.",
-			name: "PATH",
-			type: "confirm",
-			when: (answers) => answers.TYPE === "Component",
-		},
-	];
-
-	return inquirer.prompt(questions);
-};
 
 const reactCli = async () => {
 
@@ -41,17 +21,17 @@ const reactCli = async () => {
 		.option("--redux", "Add Redux to the template")
 		.parse(process.argv)
 
+	let options = {}
+
 	const { args } = program;
 
 	if (!args.length) {
-		const answers = await askQuestions()
-		console.log(answers)
+		options = await interactiveCLI(options)
 	}
 
 	const firstParam = args.shift()
 	const secondParam = args.shift()
 
-	let options = {}
 	if (program.flow) {
 		options = Object.assign(options, { flow: true })
 	}
@@ -65,15 +45,7 @@ const reactCli = async () => {
 	// Cmd reacli component <path> creates a component architecture
 	if (firstParam === "component") {
 		const path = pathModule.resolve(secondParam)
-
-		if (validatePath(path) && validateName(path)) {
-			try {
-				await createComponent(path, options)
-			} catch (error) {
-				console.log("ERROR: ", error)
-				program.outputHelp()
-			}
-		}
+		await createComponent(path, options)
 	}
 }
 
